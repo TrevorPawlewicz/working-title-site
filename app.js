d72bd1a5dac7d1c29ab8c1c0d25a3c24e92c3cbd
Trev <trevor.pawlewicz@gmail.com>
2017-01-21 13:22:35 -0500

    added SHOW route to app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var mongoose   = require("mongoose"); // object data mapper for MongoDB
 
 mongoose.connect("mongodb://localhost/my_titles");
 app.use(bodyParser.urlencoded({extended: true}));
-//app.set("view engine", "ejs"); // only needed to leave .ejs from res.render below
+//app.set("view engine", "ejs"); // only needed to leave .ejs from res.render
 
 app.get("/", function(req, res){
     res.render("landing.ejs");
@@ -14,25 +14,13 @@ app.get("/", function(req, res){
 // SCHEMA SETUP
 var titleSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 // compile schema into a model (adds methods to use with MongoDB)
 var Title = mongoose.model("Title", titleSchema);
 
-// Title.create(
-//     {
-//         name: "Everybody Wants Some!!",
-//         image: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk2NDcyNDE5N15BMl5BanBnXkFtZTgwNDA0MzQ1NzE@._V1_UY1200_CR70,0,630,1200_AL_.jpg"
-//     }, function (err, title){
-//         if (err) {
-//             console.log("---> here comes the error...");
-//             console.log(err);
-//         } else {
-//             console.log("New Title created: ");
-//             console.log(title);
-//         }
-// });
-
+//------------------------ ROUTES --------------------------------------------
 // get rout retrieves data
 app.get("/sonytitles", function(req, res){
     // get all titles from DB:
@@ -46,7 +34,7 @@ app.get("/sonytitles", function(req, res){
     });
 });
 
-// CREATE - POST route ADDS data
+// CREATE - POST route ADDS data ---------------------------------------------
 app.post("/sonytitles", function(req, res){
     // get data from ejs page:
     var name = req.body.name;
@@ -63,10 +51,15 @@ app.post("/sonytitles", function(req, res){
     });
 });
 
+// NEW - show form to create a new title -------------------------------------
 app.get("/sonytitles/new", function(req, res){
     res.render("new.ejs");
 });
 
+// SHOW - find one title by ID -----------------------------------------------
+app.get("/sonytitles/:id", function(req, res){
+
+});
 
 
 
@@ -77,10 +70,9 @@ app.get("/sonytitles/new", function(req, res){
 
 
 
-
-
-
+//------------------------- SERVER --------------------------------------------
 var port = process.env.PORT || 3000;
 app.listen(port, process.env.IP, function() {
     console.log("Server is started on port: " + port);
 });
+//=============================================================================
